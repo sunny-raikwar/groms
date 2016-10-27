3699f5f975311cbb70a788933e003981e8dacd70
Shiv-Dangi <shiv.dangi71@gmail.com>
2016-10-27 16:04:51 +0530

    models are created for agent functionality

--- ggroms/grom/models.py
+++ ggroms/grom/models.py
@@ -14,6 +14,7 @@ class StaffUser(models.Model):
 	def __str__(self):
 		return self.fullname
 
+
 class LandDetail(models.Model):
 	belonging_to = models.CharField(max_length=50)
 	ph_no = models.CharField(max_length=20)
@@ -38,5 +39,62 @@ class LandDetail(models.Model):
 		return self.survey_no
 
 
+class AgentAccount(models.Model):
+	account_number = models.CharField(max_length=20)
+
+	def __str__(self):
+		return self.account_number
+
+
+
+class AgentType(models.Model):
+	type_name = models.CharField(max_length=50)
+
+	def __str__(self):
+		return self.type_name
+
+
+class NominiDetail(models.Model):
+	MALE = "Male"
+	FEMALE = "Female"
+	gender_choices = (
+		(MALE, 'Male'),
+		(FEMALE, 'Female'),
+		)
+	fullname = models.CharField(max_length=50)
+	age = models.IntegerField()
+	gender = models.CharField(max_length=6, choices=gender_choices)
+	relation = models.CharField(max_length=20)
+	address = models.TextField()
+	contact_no = models.CharField(max_length=15)
+	email = models.EmailField(max_length=100, null=True)
+	belong_to = models.CharField(max_length=10)
+
+	def __str__(self):
+		return self.fullname
+
+
+class AgentDetail(models.Model):
+	first_name = models.CharField(max_length=20)
+	middle_name = models.CharField(max_length=20)
+	last_name = models.CharField(max_length=20)
+	father_name = models.CharField(max_length=50)
+	mother_name = models.CharField(max_length=50)
+	pancard = models.CharField(max_length=50, null=True)
+	address = models.TextField()
+	date_of_birth = models.DateField()
+	contact_no = models.CharField(max_length=15)
+	alternate_contact_no = models.CharField(max_length=15, null=True) 
+	email = models.EmailField(null=True)
+	date_of_joining = models.DateField()
+	percentage_alloted = models.FloatField()
+	Nomini_id = models.ForeignKey('NominiDetail', on_delete=models.CASCADE)
+	type_id = models.ForeignKey('AgentType')
+	introducor_id = models.ForeignKey('self')
+	introducer_percentage = models.FloatField()
+	account_id = models.ForeignKey('AgentAccount')
+
+	def __str__(self):
+		return self.first_name
 
 
