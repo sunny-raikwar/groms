e2b6446fb0345a1ab1a10475e3a0a44574ff2904
Shiv <shiv@UL-LPT-0151s-MacBook-Pro.local>
2017-05-13 23:31:47 +0530

    plotly intigration in app

--- ggroms/grom/views.py
+++ ggroms/grom/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
+import plotly.offline as opy
+import plotly.graph_objs as go
 from .models import *
 
 
@@ -36,7 +38,17 @@ def clearsession(request):
 
 
 def setting(request):
-    return render(request, 'grom/setting.html')
+    x = [-2,0,4,6,7]
+    y = [q**2-q+3 for q in x]
+    trace1 = go.Scatter(x=x, y=y, marker={'color': 'red', 'symbol': 104, 'size': "10"},
+                        mode="lines",  name='1st Trace')
+
+    data=go.Data([trace1])
+    layout=go.Layout(title="Meine Daten", xaxis={'title':'x1'}, yaxis={'title':'x2'})
+    figure=go.Figure(data=data,layout=layout)
+    div = opy.plot(figure, auto_open=False, output_type='div')
+    context = {'graph': div}
+    return render(request, 'grom/setting.html', context)
 
 
 def staffmember(request):
